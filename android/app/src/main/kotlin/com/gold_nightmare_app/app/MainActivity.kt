cad73e063654a81bf91e9a1bd4737a8551340768
Odai Abu Ameera <ODAIABUAMERA199@GMAIL.COM>
2025-09-14 15:41:15 +0300

    Added proper .gitignore

---
+++ android/app/src/main/kotlin/com/gold_nightmare_app/app/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.gold_nightmare_app.app
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterFragmentActivity() {
+}
